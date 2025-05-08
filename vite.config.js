d95cb54e22aa77c336a3378aca1813b4838fe242
Trollkopf <101181314+Trollkopf@users.noreply.github.com>
2025-05-08 13:17:09 +0200

    updated vite.config for obsolete node

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import { webcrypto } from 'node:crypto';
+
+if (!globalThis.crypto) {
+    globalThis.crypto = webcrypto;
+}
 
 export default defineConfig({
     plugins: [
         laravel({
             input: 'resources/js/app.js',
             refresh: true,
+
         }),
         vue({
             template: {
